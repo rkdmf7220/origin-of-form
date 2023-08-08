b39bb46246ee067fd6857d6fb12dc578df1087cf
2sangmin <rkdmf7220@gmail.com>
2023-08-08 17:38:47 +0900

    Add intro layout to default layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,40 @@
 <template>
   <div class="wrap">
+    <Intro :is-mount="isMount" :show-delayed="showDelayed"/>
     <GlobalNav/>
     <slot/>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "default"
-}
+import Intro from "~/layouts/intro.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "default",
+  components: {Intro},
+  data() {
+    return {
+      isMount: true,
+      showDelayed: true
+    }
+  },
+  mounted() {
+    this.isMount = this.$route.name === 'index';
+  },
+  watch: {
+    $route(to, from) {
+      if (to.path === '/' && from.path != to.path) {
+        this.isMount = true
+        setTimeout(() => this.showDelayed = true, 10)
+      } else {
+        setTimeout(() => this.isMount = false, 500)
+        this.showDelayed = false
+      }
+      console.log('path >>', to.path, from.path)
+    }
+  }
+})
 </script>
 
 <style>
---
+++ layouts/intro.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="isMount" :class="{'is-show': showDelayed}" class="intro-container">
+    intro section
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "intro",
+  props: {
+    isMount: Boolean,
+    showDelayed: Boolean
+  },
+})
+</script>
+
+<style scoped lang="scss">
+
+.intro-container {
+  width: 100%;
+  height: 0;
+  transition: height 0.3s;
+  overflow: hidden;
+  background-color: #0cf;
+
+  &.intro-container.is-show {
+    height: 100vh;
+  }
+}
+</style>
