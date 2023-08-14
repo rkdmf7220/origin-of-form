287e505394cb8b83b5bf246db7724dfbc962ce31
2sangmin <rkdmf7220@gmail.com>
2023-08-14 15:59:44 +0900

    Add `Pinia`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {defineNuxtConfig} from "nuxt/config";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true
+  },
+  modules: [
+    '@pinia/nuxt',
+  ],
+  // import: {
+  //   dirs: ['./stores']
+  // },
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      // ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
+  experimental: {
+    renderJsonPayloads: false
+  }
 })
