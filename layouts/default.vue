713daf47ef9781f10af68e1965f7967cf15092a7
2sangmin <rkdmf7220@gmail.com>
2023-09-06 18:46:07 +0900

    Remove $route to watch

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="wrap">
-    <Main @scroll.prevent="onHandleScroll" :show-delayed="showDelayed" />
-    <GlobalNav />
-    <Origin />
-    <Works />
-    <Introduction />
-    <Research />
+    <Main :show-delayed="showDelayed" />
+    <!--    <Main @scroll.prevent="onHandleScroll" :show-delayed="showDelayed" />-->
+    <div>
+      <GlobalNav />
+      <Origin />
+      <Works />
+      <Introduction />
+      <Research />
+    </div>
     <slot />
   </div>
 </template>
@@ -18,36 +21,25 @@ import Works from "~/layouts/works.vue";
 import Introduction from "~/layouts/introduction.vue";
 import Research from "~/layouts/research.vue";
 
-interface IIntroData {
-  showDelayed: boolean;
-  timeoutId: null | ReturnType<typeof setTimeout>;
-}
-
 export default defineComponent({
   name: "default",
-  components: {Research, Introduction, Works, Origin, Intro},
-  data(): IIntroData {
-    return {
-      showDelayed: true,
-      timeoutId: null
+  components: {Research, Introduction, Works, Origin, Main},
+  mounted() {
+    const preventScroll = (e: WheelEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
     };
+    // window.addEventListener("wheel", (e) => preventScroll(e), {passive: false});
   },
-  watch: {
-    $route(to, from) {
-      if (to.path === "/" && from.path != to.path) {
-        if (this.timeoutId) {
-          clearTimeout(this.timeoutId);
-        }
-        this.isMount = true;
-        setTimeout(() => this.showDelayed = true, 10);
-      } else {
-        this.timeoutId = setTimeout(() => this.isMount = false, 500);
-        this.showDelayed = false;
-      }
-    }
+  data() {
+    return {
+      isMount: true,
+      showDelayed: true,
+      timeoutId: null,
+      showGlobalNav: false
+    };
   }
 });
 </script>
 
-<style>
-</style>
+<style></style>
