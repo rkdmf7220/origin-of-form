a633342723ded519bd49cd312c04d605f70ee6e2
2sangmin <rkdmf7220@gmail.com>
2023-11-17 23:22:30 +0900

    Add next/prev button event to slide

--- layouts/works.vue
+++ layouts/works.vue
@@ -1,10 +1,13 @@
 <template>
   <div ref="works" @wheel="(e) => onScrollContent(e)" id="works" class="contents">
     <div class="works-wrap">
-      <div class="works-list">
+      <div
+        :style="{transform: `translateX(calc(-${50 * this.slideIndex}vw - ${100 * this.slideIndex}px))`}"
+        class="works-list"
+      >
         <div class="works-item">
           <div class="works-inner">
-            <div class="works-img-outer">
+            <div class="works-img-outer is-clickable">
               <img src="/images/works/01.jpg" alt="사진 1" />
             </div>
             <div class="works-caption">
@@ -20,7 +23,7 @@
         </div>
         <div class="works-item">
           <div class="works-inner">
-            <div class="works-img-outer">
+            <div class="works-img-outer is-clickable">
               <img src="/images/works/02.jpg" alt="사진 2" />
             </div>
             <div class="works-caption">
@@ -35,8 +38,12 @@
           </div>
         </div>
       </div>
+      <div class="works-btn-wrap">
+        <div @click="onClickPrevImage" class="works-image-btn prev-btn is-clickable"></div>
+        <div @click="onClickNextImage" class="works-image-btn next-btn is-clickable"></div>
+      </div>
       <div class="dim-area"></div>
-      <div class="spotlight-area"></div>
+      <div :class="{'is-slide': this.isSlide}" class="spotlight-area"></div>
     </div>
   </div>
 </template>
@@ -48,6 +55,12 @@ import {IHash} from "~/interfaces/IHash";
 
 export default defineComponent({
   name: "works",
+  data() {
+    return {
+      slideIndex: 0,
+      isSlide: false
+    };
+  },
   methods: {
     onScrollContent(e: WheelEvent) {
       const refs = this.$refs["works"] as HTMLDivElement;
@@ -55,6 +68,20 @@ export default defineComponent({
       if (result !== null) {
         this.$emit("change-hash", IHash.Works, result);
       }
+    },
+    onClickNextImage() {
+      this.slideIndex++;
+      this.isSlide = true;
+      setTimeout(() => {
+        this.isSlide = false;
+      }, 200);
+    },
+    onClickPrevImage() {
+      this.slideIndex--;
+      this.isSlide = true;
+      setTimeout(() => {
+        this.isSlide = false;
+      }, 200);
     }
   }
 });
@@ -70,7 +97,7 @@ export default defineComponent({
     height: 100vh;
     position: absolute;
     overflow: hidden;
-    background-image: url("/images/grain-texture.png");
+    background-image: url("/images/grain-texture4.jpg");
     background-size: cover;
     background-repeat: no-repeat;
 
@@ -81,6 +108,9 @@ export default defineComponent({
       flex-direction: row;
       margin-left: calc(50% - 250px);
       gap: calc(50vw - 400px);
+      position: relative;
+      z-index: 3;
+      transition: transform 0.3s ease-in-out;
 
       .works-item {
         width: 100%;
@@ -137,7 +167,44 @@ export default defineComponent({
       }
     }
 
-    .dim-area {
+    .works-btn-wrap {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      //z-index: 50;
+      left: 0;
+      top: 0;
+      right: 0;
+
+      .works-image-btn {
+        width: 300px;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        transition: all 0.3s ease-in-out;
+        z-index: 50;
+
+        &.prev-btn {
+          left: 0;
+          background: linear-gradient(90deg, rgba(0, 0, 0, 0.6) 0%, rgba(0, 0, 0, 0) 100%);
+
+          &:hover {
+            background: linear-gradient(90deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0) 100%);
+          }
+        }
+
+        &.next-btn {
+          right: 0;
+          background: linear-gradient(270deg, rgba(0, 0, 0, 0.6) 0%, rgba(0, 0, 0, 0) 100%);
+
+          &:hover {
+            background: linear-gradient(270deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0) 100%);
+          }
+        }
+      }
+    }
+
+    /*.dim-area {
       width: 100%;
       height: 100%;
       position: absolute;
@@ -164,7 +231,7 @@ export default defineComponent({
         right: 0;
         background: linear-gradient(270deg, rgba(0, 0, 0, 0.6) 0%, rgba(0, 0, 0, 0) 100%);
       }
-    }
+    }*/
 
     .spotlight-area {
       width: 100%;
@@ -173,13 +240,19 @@ export default defineComponent({
       top: 0;
       left: 0;
       right: 0;
-      z-index: -1;
+      z-index: 1;
       background: radial-gradient(
         18.37% 46.06% at 50% 45.19%,
-        rgba(255, 199, 147, 0.5) 0%,
+        rgba(255, 199, 147, 0.4) 0%,
         rgba(255, 206, 161, 0) 100%
       );
       filter: blur(90px);
+      opacity: 1;
+      transition: opacity 0.2s ease-in-out;
+
+      &.is-slide {
+        opacity: 0.3;
+      }
     }
   }
 }
