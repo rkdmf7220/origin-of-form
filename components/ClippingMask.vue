24562d17dd92ccdaa05d2e69d92bcb8fa24ba285
2sangmin <rkdmf7220@gmail.com>
2023-11-28 23:44:08 +0900

    Fix non-bouncing error in clipping mask

--- components/ClippingMask.vue
+++ components/ClippingMask.vue
@@ -144,9 +144,9 @@ export default defineComponent({
     onSetMaskInMobile() {
       this.mobileMask.stageWidth = window.outerWidth;
       this.mobileMask.stageHeight = window.outerHeight;
-      this.mobileMask.xPosition = 70 + Math.floor(Math.random() * (this.mobileMask.stageWidth - 170));
-      // 최솟값 + (화면 가로 - 공 크기 - 최솟값)
-      this.mobileMask.yPosition = 120 + Math.floor(Math.random() * (this.mobileMask.stageHeight - 220));
+      this.mobileMask.xPosition = 80 + Math.floor(Math.random() * (this.mobileMask.stageWidth - 160));
+      // (최솟값 + 보정값 10)  +  Math.floor(화면 크기 -
+      this.mobileMask.yPosition = 80 + Math.floor(Math.random() * (this.mobileMask.stageHeight - 160));
     },
     moveMask() {
       this.mobileMask.xPosition += this.mobileMask.vx;
@@ -159,7 +159,7 @@ export default defineComponent({
       const maxX = this.mobileMask.stageWidth - this.mobileMask.radius + 30;
       const minY = this.mobileMask.radius - 30;
       const maxY = this.mobileMask.stageHeight - this.mobileMask.radius - 20;
-
+      // 세로 - 원 지름 + 보정 30 - GNB 5
       if (this.mobileMask.xPosition <= minX || this.mobileMask.xPosition >= maxX) {
         this.mobileMask.vx *= -1;
         this.mobileMask.stageHeight += this.mobileMask.vx;
